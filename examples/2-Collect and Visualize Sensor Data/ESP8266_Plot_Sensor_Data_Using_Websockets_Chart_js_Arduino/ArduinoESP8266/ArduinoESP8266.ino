25a8714273607ffc8e8730ae4927b49303fe986e
AminSheykhzadeh <amin.sh.sos@gmail.com>
2023-10-15 16:21:54 +0330

    include

---
+++ examples/2-Collect and Visualize Sensor Data/ESP8266_Plot_Sensor_Data_Using_Websockets_Chart_js_Arduino/ArduinoESP8266/ArduinoESP8266.ino
@@ -0,0 +1,14 @@
+//Using NodeMCU Running on ESP8266
+//Code Learned From https://www.youtube.com/watch?v=lEVoRJSsEa8&t=1s&pp=ygUxRVNQODI2Nl9QbG90X1NlbnNvcl9EYXRhX1dpdGhfV2Vic29ja2V0c19DaGFydF9qcw%3D%3D
+
+#include <ESP8266Wifi.h>
+
+
+
+void setup () {
+
+}
+
+void loop() {
+
+}
